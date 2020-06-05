930c4624e50a70ca5240adcbdf86e6eee0b05006
Ahmad Ali <aallii300300@gmail.com>
2020-06-05 02:06:09 +0100

    design single view

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,4 +1,5 @@
 export const makeTitle = (s) => {
+    if (!s || !s.name) { return ''; }
     return `${s.name.length > 25 ? s.name.substring(0, 23) : s.name} - S${s.season < 9 ? '0' + s.season : s.season}E${s.number < 9 ? '0' + s.number : s.number}`;
 
 }
