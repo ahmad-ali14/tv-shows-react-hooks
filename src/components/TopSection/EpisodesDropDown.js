841c4dc63086f0e5664760db1a61f26154ab14c7
Ahmad Ali <aallii300300@gmail.com>
2020-06-05 14:48:58 +0100

    episodes deop down menu

---
+++ src/components/TopSection/EpisodesDropDown.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { makeTitle } from '../helpers/index';
+
+const EpisodesDropDown = ({ episodes, showSelectedShowEpisodes, singleEpisode,
+    isShowsVisible, hide, show, selectSingleEpisode }) => {
+
+
+    const fillOptions = (episodes) => {
+        return episodes.map(s => { return (<option key={s.id} value={s.id}> {makeTitle(s)} </option>) })
+    }
+
+
+    return (
+        <>
+            <select id="showSelector" autoFocus="-1" value={singleEpisode.length > 0 ? singleEpisode[0].id : ''} onChange={(e) => { hide('episodes'); show('single episode'); selectSingleEpisode(e.target.value); }}>
+                <option id="defaultShowSelection" value="All Shows" selected={isShowsVisible ? true : false} >All Episodes </option>
+                {fillOptions(episodes)}
+            </select>
+        </>
+    )
+}
+
+
+export default EpisodesDropDown;
