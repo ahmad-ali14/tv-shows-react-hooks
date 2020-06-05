5a973eeb56241b1219f2975854d39826d7eb6e97
Ahmad Ali <aallii300300@gmail.com>
2020-06-05 12:55:07 +0100

    soritng methods

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -7,4 +7,62 @@ export const makeTitle = (s) => {
 
 export const extractPhoto = (obj) => {
     return obj.image ? obj.image.medium ? obj.image.medium : obj.image.original ? obj.image.original : `${process.env.PUBLIC_URL}/no-image-found-360x260.png` : `${process.env.PUBLIC_URL}/no-image-found-360x260.png`
-} 
+}
+
+export const doSort = (v) => {
+    console.log(v);
+    if (v == 'name-a-z') {
+        return function compare(a, b) {
+            if (a.name > b.name) return 1;
+            if (b.name > a.name) return -1;
+
+            return 0;
+        }
+
+        // doingDiffSort(compare);
+
+    }
+
+    if (v == 'name-z-a') {
+        return function compare(a, b) {
+            if (b.name > a.name) return 1;
+            if (a.name > b.name) return -1;
+
+            return 0;
+        }
+
+        // doingDiffSort(compare);
+
+    }
+
+    if (v == 'rating+') {
+        return function compare(a, b) {
+            if (a.rating.average > b.rating.average) return 1;
+            if (b.rating.average > a.rating.average) return -1;
+
+            return 0;
+        }
+
+        //  doingDiffSort(compare);
+
+    }
+
+    if (v == 'rating-') {
+        return function compare(a, b) {
+            if (b.rating.average > a.rating.average) return 1;
+            if (a.rating.average > b.rating.average) return -1;
+
+            return 0;
+        }
+
+        //  doingDiffSort(compare);
+
+    }
+
+    if (v == 'no-sort') {
+        // setup();
+
+    }
+}
+
+
